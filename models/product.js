e4bd29670d2573f5db95f3c22aceab5d1c905554
aryantomar11609-lab <aryantomar11609@gmail.com>
2025-10-31 10:25:49 +0530

    Create product.js

---
+++ models/product.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const variantSchema = new mongoose.Schema({
+  color: String,
+  size: String,
+  stock: Number,
+});
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  variants: [variantSchema],
+});
+
+module.exports = mongoose.model('Product', productSchema);
